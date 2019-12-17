85e74b3e6d9617c5d37dfb26b2ca9412ff25210e
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2019-12-17 16:23:55 +0200

    Finish [SOLID] [Liskov Substitution]

---
+++ MainProject/SOLID_PRINCIPLES/LiskovSubstitution.cs
@@ -0,0 +1,60 @@
+﻿using static System.Console;
+namespace MainProject.SOLID_PRINCIPLES
+{
+    public class LiskovSubstitution
+    {
+        public static void Run()
+        {
+            WriteLine("START __ [SOLID] [LiskovSubstitution]");
+            int Area(Rectangle rect)
+            {
+                return rect.Width * rect.Height;
+            }
+            
+            Rectangle rect = new Rectangle(10,5);
+            Rectangle square = new Square();
+            square.Width = 10;
+            WriteLine($"React {rect} : Has Area = {Area(rect)}");
+            WriteLine($"Square {square} : Has Area = {Area(square)}");
+        }
+    }
+    
+    #region Base Classes
+
+    class Rectangle
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        // To allow inheritance without needing to re define constructor
+        public Rectangle()
+        {
+        }
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Width)}: {Width}, {nameof(Height)}: {Height}";
+        }
+    }
+
+    class Square: Rectangle
+    {
+        public new int Width
+        {
+            set => base.Width = base.Height = value;
+        }
+
+        public new int Height
+        {
+            set => base.Width = base.Height = value;
+        }
+    }
+
+    #endregion
+}
---
+++ MainProject/SOLID_PRINCIPLES/LiskovSubstitution.solve.cs
@@ -0,0 +1,66 @@
+﻿using static System.Console;
+namespace MainProject.SOLID_PRINCIPLES
+{
+    
+    // we should always able to upcast to the base type and behaviour should still the same
+    namespace Solve
+    {
+        public static class LiskovSubstitution
+        {
+            public static void Run()
+            {
+                WriteLine("START __ [SOLID] [LiskovSubstitution]");
+                int Area(Rectangle rect)
+                {
+                    return rect.Width * rect.Height;
+                }
+            
+                Rectangle rect = new Rectangle(10,5);
+                Rectangle square = new Square(); // upcasting
+                square.Width = 10;
+                WriteLine($"React {rect} : Has Area = {Area(rect)}");
+                WriteLine($"Square {square} : Has Area = {Area(square)}");
+            }
+        }
+    
+        #region Base Classes
+
+        class Rectangle
+        {
+            public virtual int Width { get; set; }
+            public virtual int Height { get; set; }
+
+            // To allow inheritance without needing to re define constructor
+            public Rectangle()
+            {
+            }
+
+            public Rectangle(int width, int height)
+            {
+                Width = width;
+                Height = height;
+            }
+
+            public override string ToString()
+            {
+                return $"{nameof(Width)}: {Width}, {nameof(Height)}: {Height}";
+            }
+        }
+
+        class Square: Rectangle
+        {
+            public override int Width
+            {
+                set => base.Width = base.Height = value;
+            }
+
+            public override int Height
+            {
+                set => base.Width = base.Height = value;
+            }
+        }
+
+        #endregion
+    }
+ 
+}
