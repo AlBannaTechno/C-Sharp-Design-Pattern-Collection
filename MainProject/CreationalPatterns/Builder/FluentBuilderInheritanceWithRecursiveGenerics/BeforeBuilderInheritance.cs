6052842ad35e5bd40faba06e2818a2ecaf1d062a
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2019-12-19 20:05:47 +0200

    [Start Working With ]: BuilderInheritance

---
+++ MainProject/CreationalPatterns/Builder/FluentBuilderInheritanceWithRecursiveGenerics/BeforeBuilderInheritance.cs
@@ -0,0 +1,45 @@
+﻿namespace MainProject.CreationalPatterns.Builder.FluentBuilderInheritanceWithRecursiveGenerics
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Position { get; set; }
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Position)}: {Position}";
+        }
+    }
+
+    public class PersonalInfoBuilder
+    {
+        protected Person person = new Person();
+
+        public PersonalInfoBuilder Called(string name)
+        {
+            person.Name = name;
+            return this;
+        }
+    }
+
+    public class PersonJobBuilder : PersonalInfoBuilder
+    {
+        public PersonJobBuilder WorksAs(string position)
+        {
+            person.Position = position;
+            return this;
+        }
+    }
+    public static class BeforeBuilderInheritance
+    {
+        public static void Run()
+        {
+            // var personJobBuilder = new PersonJobBuilder();
+            // personJobBuilder.Called("Osama")
+            //     .WorksAs("ss"); // we can not do this 
+            // // we not allowed to use the containing type as a return type
+            //
+            // So We need to make PersonalInfoBuilder.Called Return PersonJobBuilder Not PersonalInfoBuilder :) 
+        }
+    }
+}
