4ebdc03a1c82512c9e5e65321e6063f78217ebf8
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2019-12-20 02:59:41 +0200

    Finish [5] General FunctionalNester : Refactor & CleanUp And Organize

--- MainProject/CreationalPatterns/Builder/FunctionalNester/FunctionalNester.cs
+++ MainProject/CreationalPatterns/Builder/FunctionalNester/FunctionalNester.cs
@@ -9,64 +9,56 @@ namespace MainProject.CreationalPatterns.Builder.FunctionalNester
     {
         public readonly List<FunctionalNester<T>> Children = new List<FunctionalNester<T>>();
         public T Item { get; set; }
+
+        #region Make Next [p,f,...] public / private depends on design descessions , and I will leave all of them public 
+
         public FunctionalNester<T> Parent { get; set; }
         public bool AllowChildren { get; set; } = true;
         public bool ThrownErrorOnChildrenAdd { get; set; } = true;
         public readonly Func<FunctionalNester<T>, int ,string> ToStringImplementation;
+
+        #endregion
 
         public FunctionalNester(Func<FunctionalNester<T>, int ,string> toStringImplementation)
         {
             ToStringImplementation = toStringImplementation;
             
         }
-        private (bool allowChildrentAttributeDefined, bool allowChildrenValue, bool thrownError) IsAllowingChildren(T item)
+        private static (bool allowChildrentAttributeDefined, bool allowChildrenValue, bool thrownError)
+            IsAllowingChildren(T item)
         {
-            (bool allowChildrentAttributeDefined, bool allowChildrenValue, bool thrownError) result = (false ,false, true);
-            // we will use reflection with attributes
-            var allowChildrenField = typeof(T).GetProperties().FirstOrDefault(f => CustomAttributeExtensions.IsDefined((MemberInfo) f, typeof(AllowChildrenBinderAttribute)));
-            if (allowChildrenField != null)
-            {
-                result.allowChildrentAttributeDefined = true;
-                var allowChildren = allowChildrenField.GetValue(item);
-                result.allowChildrenValue = (bool) allowChildren;
-                var attr = (AllowChildrenBinderAttribute) allowChildrenField.GetCustomAttribute(typeof(AllowChildrenBinderAttribute));
-                result.thrownError = attr.ThrownError;
-            }
+            (bool allowChildrentAttributeDefined, bool allowChildrenValue, bool thrownError) 
+                result = (false ,false, true);
+            var allowChildrenField = typeof(T).GetProperties()
+                .FirstOrDefault(f => f.IsDefined(typeof(AllowChildrenBinderAttribute)));
+            if (allowChildrenField == null) return result;
+            result.allowChildrentAttributeDefined = true;
+            var allowChildren = allowChildrenField.GetValue(item);
+            result.allowChildrenValue = (bool) allowChildren;
+            var attr = (AllowChildrenBinderAttribute) allowChildrenField
+                .GetCustomAttribute(typeof(AllowChildrenBinderAttribute));
+            result.thrownError = attr.ThrownError;
             return result;
         }
         private bool CheckAddChildrenAvailability()
         {
-            // we may allow to not throw an error : just print warning or do nothing
-            if (!AllowChildren)
-            {
-                if (!ThrownErrorOnChildrenAdd)
-                {
-                    Console.Error.WriteLine("Please review your nester chain : because some " +
-                                            "elements will not set because its parent can not have a children");
-                    Console.Error.WriteLine("You can Also Enable Thrown Error on AllowChildrenBinder Attribute " +
-                                            "To Track This problem");
-                    return false;
-                }
-                throw new Exception("You can not add any child to this element");
-            }
-            return true;
+            if (AllowChildren) return true;
+            if (ThrownErrorOnChildrenAdd) throw new Exception("You can not add any child to this element");
+            Console.Error.WriteLine("Please review your nester chain : because some " +
+                                    "elements will not set because its parent can not have a children");
+            Console.Error.WriteLine("You can Also Enable Thrown Error on AllowChildrenBinder Attribute " +
+                                    "To Track This problem");
+            return false;
         }
         
-        private (bool allowChildrenDecision, bool thrownError) CheckAddChildrenAllowabilityOfThis(T item, bool allowChildren)
+        private static (bool allowChildrenDecision, bool thrownError) 
+            CheckAddChildrenAllowabilityOfThis(T item, bool allowChildren)
         {
-            var acResult = IsAllowingChildren(item);
+            var (allowChildrenAttributeDefined, allowChildrenValue, thrownError) 
+                = IsAllowingChildren(item);
             // decision
-            bool allowChildrenDecision;
-            if (acResult.allowChildrentAttributeDefined)
-            {
-                allowChildrenDecision = acResult.allowChildrenValue;
-            }
-            else
-            {
-                allowChildrenDecision = allowChildren;
-            }
-
-            return (allowChildrenDecision, acResult.thrownError);
+            var allowChildrenDecision = allowChildrenAttributeDefined ? allowChildrenValue : allowChildren;
+            return (allowChildrenDecision, thrownError);
         }
         public FunctionalNester<T> AddChild(T child, bool allowChildren = true)
         {
@@ -74,10 +66,12 @@ namespace MainProject.CreationalPatterns.Builder.FunctionalNester
             {
                 return this;
             }
-            var (allowChildrenDecision,thrownError) = CheckAddChildrenAllowabilityOfThis(child, allowChildren);
+            var (allowChildrenDecision,thrownError) 
+                = CheckAddChildrenAllowabilityOfThis(child, allowChildren);
             var ch = new FunctionalNester<T>(ToStringImplementation)
             {
-                Item = child, Parent = this, AllowChildren = allowChildrenDecision, ThrownErrorOnChildrenAdd = thrownError
+                Item = child, Parent = this, AllowChildren = allowChildrenDecision,
+                ThrownErrorOnChildrenAdd = thrownError
             };
             Children.Add(ch);
             return this;
@@ -89,10 +83,12 @@ namespace MainProject.CreationalPatterns.Builder.FunctionalNester
                 element = null;
                 return this;
             }
-            var (allowChildrenDecision,thrownError) = CheckAddChildrenAllowabilityOfThis(child, allowChildren);
+            var (allowChildrenDecision,thrownError) = 
+                CheckAddChildrenAllowabilityOfThis(child, allowChildren);
             var ch = new FunctionalNester<T>(ToStringImplementation)
             {
-                Item = child, Parent = this, AllowChildren = allowChildrenDecision, ThrownErrorOnChildrenAdd = thrownError
+                Item = child, Parent = this, AllowChildren = allowChildrenDecision,
+                ThrownErrorOnChildrenAdd = thrownError
             };
             Children.Add(ch);
             element = ch;
@@ -104,10 +100,12 @@ namespace MainProject.CreationalPatterns.Builder.FunctionalNester
             {
                 return this;
             }
-            var (allowChildrenDecision,thrownError) = CheckAddChildrenAllowabilityOfThis(child, allowChildren);
+            var (allowChildrenDecision,thrownError) 
+                = CheckAddChildrenAllowabilityOfThis(child, allowChildren);
             var ch = new FunctionalNester<T>(ToStringImplementation)
             {
-                Item = child, Parent = this , AllowChildren = allowChildrenDecision, ThrownErrorOnChildrenAdd = thrownError
+                Item = child, Parent = this , AllowChildren = allowChildrenDecision,
+                ThrownErrorOnChildrenAdd = thrownError
             };
             Children.Add(ch);
             return ch;
@@ -119,10 +117,12 @@ namespace MainProject.CreationalPatterns.Builder.FunctionalNester
                 element = null;
                 return this;
             }
-            var (allowChildrenDecision,thrownError) = CheckAddChildrenAllowabilityOfThis(child, allowChildren);
+            var (allowChildrenDecision,thrownError) 
+                = CheckAddChildrenAllowabilityOfThis(child, allowChildren);
             var ch = new FunctionalNester<T>(ToStringImplementation)
             {
-                Item = child, Parent = this, AllowChildren = allowChildrenDecision, ThrownErrorOnChildrenAdd = thrownError
+                Item = child, Parent = this, AllowChildren = allowChildrenDecision,
+                ThrownErrorOnChildrenAdd = thrownError
             };
             Children.Add(ch);
             element = ch;
@@ -131,12 +131,7 @@ namespace MainProject.CreationalPatterns.Builder.FunctionalNester
 
         public FunctionalNester<T> ToParent(int upTo = 1)
         {
-            if (upTo == 1)
-            {
-                return Parent;
-            }
-
-            return Parent.ToParent(upTo - 1);
+            return upTo == 1 ? Parent : Parent.ToParent(upTo - 1);
         }
 
         public override string ToString()
