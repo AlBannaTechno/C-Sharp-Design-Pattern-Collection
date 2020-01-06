aec5becf94d03dd567a23429bdb0ec00d3277fa0
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2020-01-06 21:25:39 +0200

    [Bridge] Finish Solving Bridge Cases

---
+++ MainProject/CreationalPatterns/Bridge/B_2_ImplementBridgeExtend.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Linq;
+using Autofac;
+using Autofac.Core;
+using static System.Console;
+namespace MainProject.CreationalPatterns.Bridge
+{
+    namespace B1ImplementBridgeExtend
+    {
+        /**
+         * Note :
+         * Implementing System like this is very dangerous and need to be very careful
+         * when working with and may leeds to enormous complexity
+         * Also this may violate Single Responsibility Principle
+         *
+         *  Another solution at : B3SolveTheProblemWithDifferentSolution
+         */
+        #region Render
+        interface IRender
+        {
+            void RenderCircle(float radius);
+            void RenderRectangle(float width, float height);
+        }
+
+        class VectorRender : IRender
+        {
+            public void RenderCircle(float radius)
+            {
+                WriteLine($"Drawing a Circle[Vector] With {radius} Radius");
+            }
+
+            public void RenderRectangle(float width, float height)
+            {
+                WriteLine($"Drawing a Rectangle[Vector] With : [Width : {width}, Height : {height}]");
+            }
+        }
+
+        // pixel
+        class RasterRender : IRender
+        {
+            public void RenderCircle(float radius)
+            {
+                WriteLine($"Drawing a Circle[Pixel] With {radius} Radius");
+            }
+
+            public void RenderRectangle(float width, float height)
+            {
+                WriteLine($"Drawing a Rectangle[Pixel] With : [Width : {width}, Height : {height}]");
+            }
+        }
+        #endregion
+
+        // [Bridge] build a bridge between shape and IRender
+        // [Anti- Bridge] : Make shape decide how it will render [limit]
+        abstract class Shape
+        {
+            protected  IRender Render;
+
+            protected Shape(IRender render)
+            {
+                Render = render ?? throw new ArgumentNullException();
+            }
+
+            public abstract void Draw();
+            public abstract void Resize(float factor);
+        }
+
+        class Circle : Shape
+        {
+            private float _radius;
+            public Circle(IRender render, float radius) : base(render)
+            {
+                _radius = radius;
+            }
+
+            public override void Draw()
+            {
+                Render.RenderCircle(_radius);
+            }
+
+            public override void Resize(float factor)
+            {
+                _radius *= factor;
+            }
+        }
+
+
+        class Rectangle : Shape
+        {
+            private float _width;
+            private float _height;
+
+            public Rectangle(IRender render, float width, float height) : base(render)
+            {
+                _width = width;
+                _height = height;
+            }
+
+            public override void Draw()
+            {
+                Render.RenderRectangle(_width, _height);
+            }
+
+            public override void Resize(float factor)
+            {
+                _height *= factor;
+                _width *= factor;
+            }
+        }
+        public class Demo
+        {
+            public void Run()
+            {
+
+                var cb = new ContainerBuilder();
+                cb.RegisterType<VectorRender>().As<IRender>();
+                cb.Register(((context, parameters) =>
+                    new Circle(context.Resolve<IRender>(), parameters.Named<float>("radius"))
+                    ));
+                cb.Register(((context, parameters) =>
+                {
+                    var enumerable = parameters as Parameter[] ?? parameters.ToArray();
+                    return new Rectangle(context.Resolve<IRender>(), enumerable.Positional<float>(0),
+                            enumerable.Positional<float>(1));
+                }));
+                using var c = cb.Build();
+                var circle = c.Resolve<Circle>(new NamedParameter("radius", 2.3f));
+                var rectangle = c.Resolve<Rectangle>(new PositionalParameter(0, 20.0f),new PositionalParameter(1, 30.0f));
+                circle.Draw();
+                circle.Resize(.2f);
+                circle.Draw();
+
+                rectangle.Draw();
+                rectangle.Resize(0.333f);
+                rectangle.Draw();
+
+                /*
+                // IRender render = new RasterRender();
+                IRender render = new VectorRender();();
+                var circle = new Circle(render, 10);
+                circle.Draw();
+                circle.Resize(3);
+                circle.Draw();*/
+            }
+        }
+
+
+    }
+}
---
+++ MainProject/CreationalPatterns/Bridge/B_3_SolveTheProblemWithDifferentSolution.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace MainProject.CreationalPatterns.Bridge
+{
+    namespace B3SolveTheProblemWithDifferentSolution
+    {
+        /*
+         * Render : [Vector, Raster]
+         * Shape ............................
+         */
+
+        struct Point
+        {
+            public float X;
+            public float Y;
+            public float Z;
+            public float W;
+
+            public static implicit operator Point((float x, float y , float z, float w) val)
+            {
+                return new Point() {X = val.x, Y = val.y, Z = val.z, W = val.w};
+            }
+
+        }
+        abstract class Render
+        {
+            // Center of drawing [Axis Gizmo]
+            protected (float x, float y, float z) Gizmo;
+            protected Render((float x, float y , float z) gizmo = default)
+            {
+                Gizmo = gizmo;
+            }
+            public abstract void RenderUnit(float x = 0, float y = 0, float z = 0, float w = 1);
+        }
+
+        class VectorRender : Render
+        {
+            public VectorRender((float x, float y , float z) gizmo = default) : base(gizmo)
+            {
+
+            }
+
+            public override void RenderUnit(float x = 0, float y = 0, float z = 0, float w = 1)
+            {
+                Console.Write($"Render Vector Unit : {x}, {y}, {z}, {w} ");
+                Console.WriteLine($"With Gizmo : {Gizmo.x}, {Gizmo.y}, {Gizmo.z} ");
+            }
+        }
+
+        class RasterRender : Render
+        {
+            public RasterRender((float x, float y , float z) gizmo = default) : base(gizmo)
+            { }
+
+            public override void RenderUnit(float x = 0, float y = 0, float z = 0, float w = 1)
+            {
+                Console.Write($"Render Raster Unit : {x}, {y}, {z}, {w} ");
+                Console.WriteLine($"With Gizmo : {Gizmo.x}, {Gizmo.y}, {Gizmo.z} ");
+            }
+        }
+
+        abstract class Shape
+        {
+            protected readonly Render Render;
+
+            protected Shape(Render render)
+            {
+                Render = render;
+            }
+
+            public abstract void Draw();
+            public abstract void Scale(float factor);
+        }
+
+        class Circle : Shape
+        {
+            private readonly float _radius;
+
+            public Circle(Render render, float radius) : base(render)
+            {
+                _radius = radius;
+            }
+
+            public override void Draw()
+            {
+                var permiter = 3.14 * 2 * _radius;
+                // do any calculations
+                var pointCollection = new List<(float x, float y, float z, float w)> {(_radius, 0, 0.0f, 1)};
+                // ....... add many points
+                foreach (var point in pointCollection)
+                {
+                    Render.RenderUnit(point.x, point.y, 0, 0);
+                }
+            }
+
+            public override void Scale(float factor)
+            {
+                // do stuff
+            }
+        }
+
+        public class Demo
+        {
+            public static void Run()
+            {
+                var circle = new Circle(new RasterRender(), 5.0f);
+                circle.Scale(0.3f);
+                circle.Draw();
+            }
+        }
+    }
+}
