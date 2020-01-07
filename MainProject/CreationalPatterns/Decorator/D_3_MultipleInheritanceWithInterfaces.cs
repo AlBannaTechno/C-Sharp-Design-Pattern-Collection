47ac995266248e1a8de378ae73ea943af364bf88
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2020-01-08 00:16:19 +0200

    [Decorator] Implement Multi inheritance Via interfaces

---
+++ MainProject/CreationalPatterns/Decorator/D_3_MultipleInheritanceWithInterfaces.cs
@@ -0,0 +1,75 @@
+﻿using static System.Console;
+namespace MainProject.CreationalPatterns.Decorator
+{
+    namespace D3MultipleInheritanceWithInterfaces
+    {
+        internal interface IAnimal
+        {
+            float Weight { get; set; }
+            void Walk();
+        }
+
+        class Animal : IAnimal
+        {
+            public float Weight { get; set; }
+
+            public void Walk()
+            {
+                WriteLine($"Walk With Weight {Weight}");
+            }
+        }
+
+        internal interface IBird
+        {
+            float Weight { get; set; }
+            void Fly();
+        }
+
+        class Bird : IBird
+        {
+            public float Weight { get; set; }
+
+            public void Fly()
+            {
+                WriteLine($"Fly With Weight {Weight}");
+            }
+        }
+
+        class Duck : IAnimal, IBird
+        {
+            public float Weight
+            {
+                get => _weight;
+                set
+                {
+                    _weight = value;
+                    _bird.Weight = value;
+                    _animal.Weight = value;
+                }
+            }
+
+            public void Fly()
+            {
+                _bird.Fly();
+            }
+
+            public void Walk()
+            {
+                _animal.Walk();
+            }
+
+            private Animal _animal = new Animal();
+            private Bird _bird = new Bird();
+            private float _weight;
+        }
+        public static class MultipleInheritanceWithInterfaces
+        {
+            public static void Run()
+            {
+                var duck = new Duck {Weight = 2.3f};
+                duck.Fly();
+                duck.Walk();
+            }
+        }
+    }
+}
