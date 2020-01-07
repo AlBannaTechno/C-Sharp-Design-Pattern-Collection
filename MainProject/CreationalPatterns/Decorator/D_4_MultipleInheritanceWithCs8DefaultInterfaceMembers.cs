760c43293bd8b9c9e9ce7d6df1c9e650b9f1d4b2
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2020-01-08 00:41:52 +0200

    [Decorator] Implement Multi inheritance Via default interface members

---
+++ MainProject/CreationalPatterns/Decorator/D_4_MultipleInheritanceWithCs8DefaultInterfaceMembers.cs
@@ -0,0 +1,106 @@
+﻿using static System.Console;
+namespace MainProject.CreationalPatterns.Decorator
+{
+    namespace D4MultipleInheritanceWithCs8DefaultInterfaceMembers
+    {
+        interface ICreature
+        {
+            int Age { get; set; }
+            private int Height
+            {
+                get { return Age * 2;}
+                set { /**/ }
+            }
+
+            void Speak()
+            {
+                if (Age > 1000) // :)
+                {
+                    WriteLine("Start Speaking :))))");
+                }
+                else
+                {
+                    Age += 10;
+                }
+            }
+
+        }
+        internal interface IAnimal : ICreature
+        {
+            float Weight { get; set; }
+            public void Walk()
+            {
+                WriteLine($"Walk With Weight {Weight}");
+            }
+
+        }
+
+        class Animal : IAnimal
+        {
+            public float Weight { get; set; }
+
+            public int Age { get; set; }
+        }
+
+        internal interface IBird : ICreature
+        {
+            float Weight { get; set; }
+            void Fly();
+        }
+
+        class Bird : IBird
+        {
+            public float Weight { get; set; }
+
+            public void Fly()
+            {
+                WriteLine($"Fly With Weight {Weight}");
+            }
+
+            public int Age { get; set; }
+        }
+
+        class Duck : IAnimal, IBird
+        {
+            public float Weight
+            {
+                get => _weight;
+                set
+                {
+                    _weight = value;
+                    _bird.Weight = value;
+                    _animal.Weight = value;
+                }
+            }
+
+            public void Fly()
+            {
+                _bird.Fly();
+            }
+
+            private Animal _animal = new Animal();
+            private Bird _bird = new Bird();
+            private float _weight;
+            public int Age { get; set; }
+        }
+
+        public static class MIDCDM
+        {
+            public static void Run()
+            {
+                var duck = new Duck();
+                ((ICreature)duck).Speak();
+                (duck as ICreature).Speak();
+                if (duck is ICreature c)
+                {
+                    c.Speak();
+                }
+
+                if (duck is IAnimal animal)
+                {
+                    animal.Walk();
+                }
+            }
+        }
+    }
+}
